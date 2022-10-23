ddc755b6ed6c863df1ae0a3bc2436610a0624a7c
Yih Yang <yyngo788n88@hotmail.com>
2022-10-24 02:22:52 +0800

    initialized repository

---
+++ index.js
@@ -0,0 +1,158 @@
+/**
+ * Check whether the list is exhaustive
+ *
+ * @param  {array}   list    List to be checked, should be in an array of objects
+ *                           in the following format: {'min':1, 'max': 2}
+ * @param  {object}  options options for the checking operations
+ *
+ * @return {Boolean}         result
+ */
+function isExhaustiveList(list = [], options = {}) {
+  // check whether list is an array
+  if (!Array.isArray(list)) {
+    throw "First argument must be an array";
+  }
+
+  // check whether options is an object
+  if (typeof options != "object" || options === null) {
+    throw "Second argument must be an object";
+  }
+
+  // check whether the list is valid
+  if (!checkIsValidList(list)) {
+    throw 'The items inside list must contain both "min" and "max" key';
+  }
+
+  // if the list is empty or only contains 1 item, return true
+  if ([0, 1].includes(list.length)) {
+    return true;
+  }
+
+  // check whether is it sorted
+  if (options.isSorted === true && !checkIsSorted(list)) {
+    // throw 'The list is not sorted when the "isSorted" flag is true'
+
+    return {
+      result: false,
+      errors: {
+        all: ['The list is not sorted when the "isSorted" flag is true'],
+      },
+    };
+  }
+
+  list = list.map((item, index) => {
+    item.original_index = index;
+    return item;
+  });
+
+  // sort the list
+  let sortedList = list.sort((a, b) => a.min - b.min);
+
+  let errors = {};
+  let previousItem = null;
+
+  for (let i = 0; i < sortedList.length; i++) {
+    let currentItem = sortedList[i];
+    // check whether min > max
+    if (currentItem.min > currentItem.max) {
+      errors = addErrors(
+        errors,
+        currentItem.original_index,
+        '"min" value is greater than "max"'
+      );
+    }
+    // if previous item exists (not first element), compare
+    if (previousItem) {
+      // check whether there's overlap
+      if (previousItem.max > currentItem.min) {
+        errors = addErrors(
+          errors,
+          previousItem.original_index,
+          '"max" value is greater than "min" value with the item in next order'
+        );
+        errors = addErrors(
+          errors,
+          currentItem.original_index,
+          '"min" value is smaller than "max" value with the item in previous order'
+        );
+      }
+
+      //
+      if (currentItem.min - previousItem.max > 1) {
+        errors = addErrors(
+          errors,
+          previousItem.original_index,
+          'There is a gap between the current item "max" value with the "min" value of item in next order'
+        );
+        errors = addErrors(
+          errors,
+          currentItem.original_index,
+          'There is a gap between the current item "min" value with the "max" value of item in previous order'
+        );
+      }
+    }
+
+    // move on with next item
+    previousItem = currentItem;
+  }
+
+  if (Object.keys(errors).length) {
+    return {
+      result: false,
+      errors,
+    };
+  }
+
+  return {
+    result: true,
+  };
+}
+
+/**
+ * Check whether the list is valid (contains "min" and "max" key)
+ *
+ * @param  {array}    list list
+ *
+ * @return {Boolean}       result
+ */
+function checkIsValidList(list) {
+  for (const item of list) {
+    if (typeof item.min === "undefined" || typeof item.max === "undefined") {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * Check whether the list is sorted
+ *
+ * @param  {array}    list list
+ *
+ * @return {Boolean}       result
+ */
+function checkIsSorted(list) {
+  let currentValue = list[0].min;
+  for (const item of list) {
+    if (item.min <= currentValue) {
+      return false;
+    }
+
+    currentValue = item.min;
+  }
+
+  return true;
+}
+
+function addErrors(errors, index, message) {
+  let itemError = errors[index] || [];
+  itemError.push(message);
+  errors[index] = itemError;
+
+  return errors;
+}
+
+module.exports = {
+  isExhaustiveList,
+};
---
+++ index.test.js
@@ -0,0 +1,148 @@
+const { isExhaustiveList } = require("./index");
+
+test("Will throw exception if first argument is not array", () => {
+  expect(() => {
+    isExhaustiveList(null);
+  }).toThrow("First argument must be an array");
+});
+
+test("Will throw exception if second argument is not object", () => {
+  expect(() => {
+    isExhaustiveList([], null);
+  }).toThrow("Second argument must be an object");
+});
+
+test("Will return true if provided list is empty", () => {
+  expect(isExhaustiveList([])).toBe(true);
+});
+
+test("Will return true if provided list contains only 1 item", () => {
+  expect(isExhaustiveList([{ min: 1, max: 2 }])).toBe(true);
+});
+
+test('Will throw expection if provided list is do not contain "min" and "max" key', () => {
+  expect(() => {
+    isExhaustiveList([{}]);
+  }).toThrow('The items inside list must contain both "min" and "max" key');
+  expect(() => {
+    isExhaustiveList([{ min: 1 }]);
+  }).toThrow('The items inside list must contain both "min" and "max" key');
+  expect(() => {
+    isExhaustiveList([{ max: 2 }]);
+  }).toThrow('The items inside list must contain both "min" and "max" key');
+});
+
+test('Will not throw expection if provided list contains both "min" and "max" key', () => {
+  expect(() => {
+    isExhaustiveList([{ min: 1, max: 2 }]);
+  }).not.toThrow('The items inside list must contain both "min" and "max" key');
+});
+
+test('Will return error if options "isSorted" is set to true but the list is not sorted', () => {
+  expect(
+    isExhaustiveList(
+      [
+        { min: 2, max: 3 },
+        { min: 0, max: 1 },
+      ],
+      {
+        isSorted: true,
+      }
+    )
+  ).toStrictEqual({
+    result: false,
+    errors: {
+      all: ['The list is not sorted when the "isSorted" flag is true'],
+    },
+  });
+});
+
+test('Will not throw expection if options "isSorted" is set to false and the list is not sorted', () => {
+  expect(
+    isExhaustiveList(
+      [
+        { min: 2, max: 3 },
+        { min: 0, max: 1 },
+      ],
+      {
+        isSorted: false,
+      }
+    )
+  ).not.toBe({
+    result: false,
+  });
+});
+
+test("Will return error if list items min value is greater than max value", () => {
+  expect(
+    isExhaustiveList(
+      [
+        { min: 4, max: 3 },
+        { min: 2, max: 1 },
+      ],
+      {
+        isSorted: false,
+      }
+    )
+  ).toStrictEqual({
+    result: false,
+    errors: {
+      0: [
+        '"min" value is greater than "max"',
+        'There is a gap between the current item "min" value with the "max" value of item in previous order',
+      ],
+      1: [
+        '"min" value is greater than "max"',
+        'There is a gap between the current item "max" value with the "min" value of item in next order',
+      ],
+    },
+  });
+});
+
+test("Will return error if list items overlaps with one another", () => {
+  expect(
+    isExhaustiveList(
+      [
+        { min: 1, max: 2 },
+        { min: 0, max: 2 },
+      ],
+      {
+        isSorted: false,
+      }
+    )
+  ).toStrictEqual({
+    result: false,
+    errors: {
+      0: [
+        '"min" value is smaller than "max" value with the item in previous order',
+      ],
+      1: [
+        '"max" value is greater than "min" value with the item in next order',
+      ],
+    },
+  });
+});
+
+test("Will return error if list items has a gap with the next item", () => {
+  expect(
+    isExhaustiveList(
+      [
+        { min: 4, max: 5 },
+        { min: 0, max: 2 },
+      ],
+      {
+        isSorted: false,
+      }
+    )
+  ).toStrictEqual({
+    result: false,
+    errors: {
+      0: [
+        'There is a gap between the current item "min" value with the "max" value of item in previous order',
+      ],
+      1: [
+        'There is a gap between the current item "max" value with the "min" value of item in next order',
+      ],
+    },
+  });
+});
